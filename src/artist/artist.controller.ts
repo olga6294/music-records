c62495cf68c1472e408c842034152d7eb65ca235
Olga <olga@MacBook-Pro-Olga.local>
2022-07-19 21:54:01 +0200

    add mocks

--- src/artist/artist.controller.ts
+++ src/artist/artist.controller.ts
@@ -6,9 +6,9 @@ export class ArtistController {
     router = Router();
 
     constructor(private artistService: ArtistService) {
-        this.router.get('/artist', (request: Request, response: Response, next: NextFunction) => this.getAllByGenre(request.query.genre as string));
-        this.router.get('/artist/stats', (request: Request, response: Response, next: NextFunction) => this.getStats());
-        this.router.get('/artist/:id/albums', validateArtistIdParam, (request: Request, response: Response, next: NextFunction) => this.getAllAlbums(+request.params.id));
+        this.router.get('/artist', (request: Request, response: Response, next: NextFunction) => response.json(this.getAllByGenre(request.query.genre as string)));
+        this.router.get('/artist/stats', (request: Request, response: Response, next: NextFunction) => response.json(this.getStats()));
+        this.router.get('/artist/:id/albums', validateArtistIdParam, (request: Request, response: Response, next: NextFunction) => response.json(this.getAllAlbums(+request.params.id)));
     }
 
     getAllByGenre(genre: string) {
--- src/artist/artist.repository.ts
+++ src/artist/artist.repository.ts
@@ -2,22 +2,35 @@ import databaseConnection from "../db/sqlite.connection";
 
 export class ArtistRepository {
 
-    getAllByGenre = (genre: string) => databaseConnection.prepare(
-        "SELECT ArtistId, Name FROM artists " +
-        "JOIN albums ON artists.ArtistId = albums.ArtistId " +
-        "JOIN tracks ON albums.AlbumId = tracks.AlbumId " +
-        "JOIN genres ON tracks.GenreId = genres.GenreId " +
-        "WHERE genres.Name = ? ORDER BY genres.Name DESC").get(genre);
+    getAllByGenre = (genre: string) => [{
+        id: 1,
+        name: "Sting"
+    },
+    {
+        id: 2,
+        name: "The Police"
+    }];
 
-    getStats = () => databaseConnection.prepare(
-        "SELECT artists.ArtistId, artists.Name, count(tracks.TrackId), Name FROM artists " +
-        "JOIN albums ON artists.ArtistId = albums.ArtistId " +
-        "JOIN tracks ON albums.AlbumId = tracks.AlbumId " +
-        "GROUP BY artists.ArtistId");
+    getStats = () => [{
+        id: 1,
+        name: "Sting",
+        count: 2
+    },
+    {
+        id: 2,
+        name: "The Police",
+        count: 3
+    }];
 
-    getAllAlbums = (id: number) => databaseConnection.prepare(
-        "SELECT artists.ArtistId, artists.Name, albums.Title FROM artists " +
-        "JOIN albums ON artists.ArtistId = albums.ArtistId " +
-        "WHERE AlbumId = ?").get(id);
+    getAllAlbums = (id: number) => [{
+        id: 1,
+        name: "Sting",
+        title: "Shape Of My Heart"
+    },
+    {
+        id: 2,
+        name: "The Police",
+        title: "Every Breath You Take"
+    }];
 
 }
